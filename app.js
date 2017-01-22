a394dc6b4ecd4e803a1e0a54e98b2ac598f86b67
Arxel <artur.justynski@gmail.com>
2017-01-22 16:21:18 +0100

    week1

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-!function(){"use strict";function n(n){function t(n){for(var t=[],e=0;e<n.length;e++)n[e]&&t.push(n[e]);return t}var e=[];n.lunchItems="",n.msg="",n.validData,n.checkLunch=function(){return e=t(n.lunchItems.split(",")),""==e?(n.msg="Please enter data first",void(n.validData=!1)):(n.validData=!0,void(n.msg=e.length<=3?"Enjoy!":"Too much!"))}}angular.module("LunchCheck",[]).controller("LunchCheckController",n),n.$inject=["$scope"]}();
+(function () {
+    'use strict';
+
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope) {
+
+        var items = [];
+        $scope.lunchItems = "";
+        $scope.msg = "";
+        $scope.validData;
+
+        function cleanArray(array) {
+            var cleanArray = [];
+            for (var i = 0; i < array.length; i++) {
+                if (array[i]) {
+                    cleanArray.push(array[i]);
+                }
+            }
+            return cleanArray;
+        }
+
+        $scope.checkLunch = function () {
+            items = cleanArray($scope.lunchItems.split(","));
+            if (items == "") {
+                $scope.msg = "Please enter data first";
+                $scope.validData = false;
+                return;
+            }
+
+            $scope.validData = true;
+            $scope.msg = items.length <= 3 ? "Enjoy!" : "Too much!";
+        }
+
+    }
+
+})();
